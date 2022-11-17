a36a626afe80b48358785a5734ac9be209cfe790
luiscar05 <penaabellaluiscarlos@gmail.com>
2022-11-17 11:50:21 -0500

    js

---
+++ js/media_query.js
@@ -0,0 +1,26 @@
+let barMenu = document.getElementById("barMenu");
+let main_Menu = document.getElementById("mainMenu");
+
+/* barMenu.addEventListener("click",function() {
+    if (main_Menu.classList.contains("main_Menu")) {
+        main_Menu.classList.add("main-menu-visible");
+        main_Menu.classList.remove("main-menu");
+    }
+    else{
+        main_Menu.classList.remove("main-menu-visible");
+        main_Menu.classList.add("main-menu");
+    }
+    
+}) */
+
+barMenu.addEventListener("click", function() {
+    alert();
+    if (main_Menu.classList.contains("main-Menu")) {
+        main_Menu.classList.add("main-menu-visible");
+        main_Menu.classList.remove("main-menu");
+    }
+    else{
+        main_Menu.classList.remove("main-menu-visible");
+        main_Menu.classList.add("main-menu");
+    }
+});
