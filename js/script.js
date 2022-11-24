18576e63a50e15c7be76be7ffccdfc31768352e2
luiscar05 <penaabellaluiscarlos@gmail.com>
2022-11-24 10:08:21 -0500

    fondos

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 /*VARIABLE DECLARADA PERO NO INICIALIZADA*/
-let botonMenu =0;
+let barMenu = document.getElementById("barMenu");
+let main_Menu = document.getElementById("menu-desplegable");
+barMenu.addEventListener("click", function() {
+    if (main_Menu.classList.contains("main-menu")) {
+        main_Menu.classList.add("main-menu-visible");
+        main_Menu.classList.remove("main-menu");
+    }
+    else{
+        main_Menu.classList.remove("main-menu-visible");
+        main_Menu.classList.add("main-menu");
+    }
+});
