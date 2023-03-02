cd80763a22b0905e70284baa9747d479f7d26992
alejandroDpAL <alejandropasaje799@gmail.com>
2023-03-02 11:50:48 -0500

    maquetacion nueva

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*VARIABLE DECLARADA PERO NO INICIALIZADA*/
-let barMenu = document.getElementById("barMenu");
+/* let barMenu = document.getElementById("barMenu");
 let main_Menu = document.getElementById("menu-desplegable");
 barMenu.addEventListener("click", function() {
     if (main_Menu.classList.contains("main-menu")) {
@@ -10,4 +10,4 @@ barMenu.addEventListener("click", function() {
         main_Menu.classList.remove("main-menu-visible");
         main_Menu.classList.add("main-menu");
     }
-});
+}); */
