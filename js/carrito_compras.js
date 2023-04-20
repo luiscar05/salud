484649a33582622c0daf067db51daf01de4f40b4
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-04-20 11:37:56 -0500

    actualizacion_carros

--- js/carrito_compras.js
+++ js/carrito_compras.js
@@ -7,6 +7,16 @@ let imgMini1=document.getElementById("imgMini1")
 let imgMini2=document.getElementById("imgMini2")
 let imgMini3=document.getElementById("imgMini3")
 let imgMini4=document.getElementById("imgMini4")
+let btnCerrar=document.getElementById("cerrar")
+let LighBox= document.getElementById("lightBox")
+
+btnCerrar.addEventListener("click",function () {
+  LighBox.classList.add("ligh-hidden")
+})
+imgPrincipal.addEventListener("click",function () {
+    LighBox.classList.remove("ligh-hidden")
+    
+})
 imgMini1.addEventListener("click",function () {
     imgPrincipal.setAttribute("src","img/image-product-1.jpg");  
     BoxMini1.classList.add("box-img-mini")
@@ -57,29 +67,53 @@ let btnSumar=document.getElementById("sumaObj")
 let contObj=document.getElementById("cantidad_obj")
 let cantBox=document.getElementById("cantBox")
 let numElementos= document.getElementById("numeroElement")
+let CostoProducto=document.getElementById("costoPro")
 
 btnRestar.addEventListener("click",function(){
     
     let numElementosTxt=document.getElementById("numeroElement").innerHTML
+    let CostoProductoTxt=document.getElementById("costoPro").innerHTML
+    
     if (numElementosTxt>0) {
         numElementos.innerHTML=""
         let elemntInt=parseInt(numElementosTxt)
         let totalElementosInt=elemntInt-1
         let totalElementosTxt=totalElementosInt.toString()
         numElementos.innerHTML=totalElementosTxt 
+
+        let CostoProductoInt= parseInt(CostoProductoTxt)
+        let TotalProductoInt= 85000*totalElementosInt
+        let totalProductoTxt=TotalProductoInt.toString()
+        totalProductoTxt="$"+ new Intl.NumberFormat().format(totalProductoTxt)
+        CostoProducto.innerHTML=totalProductoTxt
+
+
     }
 })
 btnSumar.addEventListener("click",function(){
     let numElementosTxt=document.getElementById("numeroElement").innerHTML
+    let CostoProductoTxt=document.getElementById("costoPro").innerHTML
     if (numElementosTxt<10) {
         numElementos.innerHTML=""
         let elemntInt=parseInt(numElementosTxt)
         let totalElementosInt=elemntInt+1
         let totalElementosTxt=totalElementosInt.toString()
         numElementos.innerHTML=totalElementosTxt  
+
+
+        let CostoProductoInt= parseInt(CostoProductoTxt)
+        let TotalProductoInt= 85000*totalElementosInt
+        let totalProductoTxt=TotalProductoInt.toString()
+        totalProductoTxt= "$ "+new Intl.NumberFormat().format(totalProductoTxt)
+        CostoProducto.innerHTML=totalProductoTxt
     }
 })
 let ConfirCompra=document.getElementById("ConfirCompra")
+let ProductAdd = document.getElementById("Product-add") 
+let CantPro=document.getElementById("numeroElement").innerHTML
 ConfirCompra.addEventListener("click",function(){
     alert ("Producto Agregado Al Carrito")
+    ProductAdd.classList.remove("product-add")
+    ProductAdd.classList.add("product-add-activo")
+    ProductAdd.innerHTML=CantPro
 })
