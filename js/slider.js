e62d0a881de1b5629a7530275e52cadc89e1c45e
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-05-04 11:45:59 -0500

    slider

---
+++ js/slider.js
@@ -0,0 +1,6 @@
+let leftArrow=document.getElementById("leftArrow")
+let MainImg=document.getElementById("paisaje1")
+leftArrow.addEventListener("click",function () {
+    MainImg.classList.remove("paisaje-1")
+    MainImg.classList.add("paisaje-1-hidden")
+})
