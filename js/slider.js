f344c52a5c4d322a97ea71c57be1a5b76f4c1b48
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-05-11 11:31:06 -0500

    slider-img

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,60 @@
-let leftArrow=document.getElementById("leftArrow")
-let MainImg=document.getElementById("paisaje1")
-leftArrow.addEventListener("click",function () {
-    MainImg.classList.remove("paisaje-1")
-    MainImg.classList.add("paisaje-1-hidden")
+let leftArrow=document.getElementById("leftArrow");
+let rightArrow=document.getElementById("rightArrow");
+
+let MainImg=document.getElementById("paisaje1");
+let MainImg2=Document.getElementById("paisaje2");
+let MainImg3=document.getElementById("paisaje3");
+let contador=0;
+alert("hola");
+leftArrow.addEventListener("click",function(){
+    alert("si")
+    if (contador===1) { 
+        MainImg.classList.remove("paisaje-1")
+        MainImg.classList.add("paisaje-1-hidden")
+        MainImg2.classList.add("paisaje-2")
+        MainImg2.classList.remove("paisaje-2-hadden")  
+        MainImg3.classList.add("paisaje-3-hidden")
+        contador=2  
+    }
+    else if (contador===2) {
+        MainImg.classList.remove("paisaje-1")
+        MainImg.classList.add("paisaje-1-hidden")
+        MainImg2.classList.remove("paisaje-2")
+        MainImg2.classList.add("paisaje-2-hidden") 
+        MainImg3.classList.add("paisaje-3")
+        MainImg3.classList.remove("paisaje-3-hidden")  
+        contador=3  
+    }
+    if (contador===3) {
+        MainImg3.classList.add("paisaje-3-hidden")
+        MainImg3.classList.remove("paisaje-3")
+        MainImg2.classList.remove("paisaje-2")
+        MainImg2.classList.add("paisaje-2-hidden")
+        MainImg.classList.add("paisaje-1")
+        MainImg.classList.remove("paisaje-1-hidden")
+        contador=1;    
+    } 
 })
-function sliders() {
     
-}
+    /* 
+       */ 
+    
+
+/* rightArrow.addEventListener("click",function () {
+    if (MainImg.classList.contains("paisaje-1")) { 
+        MainImg.classList.remove("paisaje-1")
+        MainImg.classList.add("paisaje-1-hidden")
+        MainImg3.classList.add("paisaje-3")
+        MainImg3.classList.remove("paisaje-3-hidden-left") 
+    }else if ( MainImg2.classList.contains("paisaje-2")) {
+        MainImg2.classList.remove("paisaje-2")
+        MainImg2.classList.add("paisaje-2-hidden-left") 
+        MainImg3.classList.add("paisaje-1")
+        MainImg3.classList.remove("paisaje-1-hidden") 
+    }else{
+        MainImg.classList.remove("paisaje-1")
+        MainImg.classList.add("paisaje-1-hidden")
+        MainImg2.classList.add("paisaje-2")
+        MainImg2.classList.remove("paisaje-2-hidden") 
+    }
+}) */
