43632a8afd0b4daa6453e8dd39f3116ba456a01e
luiacar05 <penaabellaluiscarlos@gmail.com>
2023-08-30 23:58:06 -0500

    chat_ana_2.0

--- js/filtro_busqueda.js
+++ js/filtro_busqueda.js
@@ -4,8 +4,6 @@ let MainBoxtwo=document.getElementById("MeinBoxTwo");
 
 let X=document.getElementById("elimanar");
 
-
-
 question.addEventListener("click",()=>{
     MeinBoxOne.classList.remove("menu");
     MeinBoxOne.classList.add("menu-inactivo");
@@ -18,4 +16,100 @@ question.addEventListener("click",()=>{
     MainBoxtwo.classList.add("menu-Busqueda-inactivo");
     MainBoxtwo.classList.remove("menu-Busqueda")
     })
+})
+
+function mostrarHora() {
+    // Obtén la hora actual
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+  
+    // Formatea la hora para que siempre tenga dos dígitos
+    hora = hora < 10 ? "0" + hora : hora;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    
+    // Construye la cadena de texto con la hora
+    var periodo = hora >= 12 ? "P.M" : "A.M";
+    if (hora > 12) {
+        hora = hora - 12; // Convierte la hora a formato de 12 horas
+    } else if (hora === 0) {
+        hora = 12; // 00:00 se muestra como 12:00 AM
+    }
+
+    var horaActual = hora + ":" + minutos + " " + periodo;
+  
+    // Actualiza el contenido del elemento HTML con la hora
+    document.getElementById("hora").innerText = horaActual;
+}
+  
+// Actualiza la hora cada segundo (1000 milisegundos)
+setInterval(mostrarHora, 1000);
+
+
+const slider= document.querySelector("#slider");
+let sliderSection=document.querySelectorAll(".box-slider");
+let sliderSectionLast=sliderSection[sliderSection.length-1];
+const btnLeft=document.querySelector(".icon-arrow-left");
+const btnRigth=document.querySelector(".icon-arrow-right");
+
+slider.insertAdjacentElement("afterbegin",sliderSectionLast);
+
+function moverDerecha() {
+    let sliderSectionFirst=document.querySelectorAll(".box-slider")[0];
+    slider.style.marginLeft="-200%";
+    slider.style.transition="all 0.5s";
+    setTimeout(() => {
+        slider.style.transition="none";
+        slider.insertAdjacentElement("beforeend",sliderSectionFirst);
+        slider.style.marginLeft="-100%";
+    }, 500);
+};
+function moverIzquierda() {
+    let sliderSection=document.querySelectorAll(".box-slider");
+    let sliderSectionLast=sliderSection[sliderSection.length-1];
+    slider.style.marginLeft="0%";
+    slider.style.transition="all 0.5s";
+    setTimeout(() => {
+        slider.style.transition="none";
+        slider.insertAdjacentElement("afterbegin",sliderSectionLast);
+        slider.style.marginLeft="-100%";
+    }, 500);
+};
+btnRigth.addEventListener("click",function () {
+    moverDerecha();
+});
+btnLeft.addEventListener("click",function () {
+    moverIzquierda();
+});
+
+
+let Form_ana=document.getElementById("form-ana");
+let btnCerrar=document.getElementById("close");
+let Form_ana_inactivo=document.querySelector(".form-ana-inactivo")
+btnCerrar.addEventListener("click",()=>{
+    let boton=document.querySelector(".exit-style")
+    if(boton.innerHTML==="Ana"){
+        boton.innerHTML="x"
+        boton.style.width="45px"
+        boton.style.borderRadius = "50%";
+        boton.style.fontSize = "2em";
+        boton.style.transition="all 0.5s";
+    }else{
+        boton.innerHTML="Ana";
+        boton.style.width="150px"
+        boton.style.borderRadius = "7px";
+        boton.style.transition="all 0.5s";
+    }
+})
+btnCerrar.addEventListener("click",()=>{
+    if (Form_ana.classList.contains("form-ana")) {
+        Form_ana.classList.add("form-ana-inactivo")
+        Form_ana.classList.remove("form-ana")
+    }else{
+        Form_ana.classList.remove("form-ana-inactivo")
+        Form_ana.classList.add("form-ana") 
+        
+    }
+    
 })
--- js/horas.js
+++ js/horas.js
@@ -1,17 +1,3 @@
-/* 
-function Hora() {
-let fecha= new Date();
-let horaDia=fecha.getHours();
-let MinutosDia=fecha.getMinutes();
-let SegundosDia=fecha.getSeconds();
-
-  horaDia = horaDia < 10 ? "0" +  horaDia :  horaDia;
-  MinutosDia = MinutosDia < 10 ? "0" + MinutosDia : MinutosDia;
-  SegundosDia = SegundosDia < 10 ? "0" + SegundosDia : SegundosDia;
-
-let HoraActual= horaDia + ":" + MinutosDia + ":" + SegundosDia;
-document.getElementById("horas").innerHTML=HoraActual
-}setInterval(hora,1000) */
 function mostrarHora() {
     // Obtén la hora actual
     var fecha = new Date();
@@ -22,13 +8,13 @@ function mostrarHora() {
     // Formatea la hora para que siempre tenga dos dígitos
     hora = hora < 10 ? "0" + hora : hora;
     minutos = minutos < 10 ? "0" + minutos : minutos;
-    segundos = segundos < 10 ? "0" + segundos : segundos;
+    
   
     // Construye la cadena de texto con la hora
     if (hora>=12) {
-        var horaActual = hora + ":" + minutos + ":" + segundos+" P.M"
+        var horaActual = hora + ":" + minutos + "P.M"
     }else{
-        var horaActual = hora + ":" + minutos + ":" + segundos+" A.M"
+        var horaActual = hora + ":" + minutos + " A.M"
     }
 
     
